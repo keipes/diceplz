9bdab4c9e2b74bcc66bd7dd381c2d88c4ccb762c
keipes <1400055+keipes@users.noreply.github.com>
2024-01-01 18:55:47 -0800

    Format some .ts files.

--- src/WeaponData.ts
+++ src/WeaponData.ts
@@ -1,49 +1,45 @@
-import weaponData from './assets/weapons.json'
+import weaponData from "./assets/weapons.json";
 
 interface Weapon {
-    name: string,
-    stats: WeaponStats[]
+  name: string;
+  stats: WeaponStats[];
 }
 
 interface WeaponStats {
-    name: string,
-    barrelType: string,
-    ammoType: string,
-    dropoffs: DamageRange[],
-    rpmSingle: number,
-    rpmBurst: number,
-    rpmAuto: number,
-    velocity: number
+  name: string;
+  barrelType: string;
+  ammoType: string;
+  dropoffs: DamageRange[];
+  rpmSingle: number;
+  rpmBurst: number;
+  rpmAuto: number;
+  velocity: number;
 }
 
 interface DamageRange {
-    damage: number,
-    range: number
+  damage: number;
+  range: number;
 }
 
 const WeaponCategories: string[] = Object.keys(weaponData);
 function GetCategoryWeapons(category: string): Weapon[] {
-    return weaponData[category];
+  return weaponData[category];
 }
 
 const weaponsByName = new Map<string, Weapon>();
 for (const category of WeaponCategories) {
-    for (const weapon of GetCategoryWeapons(category)) {
-        weaponsByName.set(weapon.name, weapon);
-    }
+  for (const weapon of GetCategoryWeapons(category)) {
+    weaponsByName.set(weapon.name, weapon);
+  }
 }
 function GetWeaponByName(name: string): Weapon {
-    return weaponsByName.get(name)!;
+  return weaponsByName.get(name)!;
 }
 export {
-    WeaponCategories,
-    GetCategoryWeapons,
-    GetWeaponByName
-    // WeaponStats,
-}
+  WeaponCategories,
+  GetCategoryWeapons,
+  GetWeaponByName,
+  // WeaponStats,
+};
 
-export type {
-    Weapon,
-    WeaponStats,
-    DamageRange
-}
+export type { Weapon, WeaponStats, DamageRange };
