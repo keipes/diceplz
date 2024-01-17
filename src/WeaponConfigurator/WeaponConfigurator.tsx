2d58d9ba742e1290c564bcc14c4f75d0d0b4ebb3
keipes <1400055+keipes@users.noreply.github.com>
2024-01-16 21:21:10 -0800

    Resizable configurator.

--- src/WeaponConfigurator/WeaponConfigurator.tsx
+++ src/WeaponConfigurator/WeaponConfigurator.tsx
@@ -1,6 +1,7 @@
 import "./WeaponConfigurator.css";
 import { WeaponConfig } from "../App";
 import Weapon from "./Weapon";
+import { SyntheticEvent, useState } from "react";
 
 interface WeaponConfiguratorProps {
   configurations: Map<string, WeaponConfiguration>;
@@ -20,8 +21,31 @@ interface WeaponConfiguration {
   ammoType: string;
 }
 
+interface MouseEventHandler {
+  (e: MouseEvent): void;
+}
+
+// assign mousemove and mouseup handlers to window, so
+// dragging can continue even if the mouse leaves the
+// configurator
+let mouseMoveHandler: MouseEventHandler | null = null;
+window.addEventListener("mousemove", (e) => {
+  if (mouseMoveHandler) {
+    mouseMoveHandler(e);
+  }
+});
+
+let mouseUpHandler: MouseEventHandler | null = null;
+window.addEventListener("mouseup", (e) => {
+  if (mouseUpHandler) {
+    mouseUpHandler(e);
+  }
+});
+
 function WeaponConfigurator(props: WeaponConfiguratorProps) {
   const weaponsDisplay = [];
+  const [dragging, setDragging] = useState(false);
+  const [height, setHeight] = useState(window.innerHeight / 3);
   for (const [id, config] of props.configurations) {
     weaponsDisplay.push(
       <Weapon
@@ -38,23 +62,47 @@ function WeaponConfigurator(props: WeaponConfiguratorProps) {
 
   let containerClass = "wcf-container";
   let expansionSymbol = "expand_more";
+  const containerStyle = {
+    height: height + "px",
+  };
   if (!props.open) {
     expansionSymbol = "expand_less";
     containerClass = "wcf-container-closed";
+    containerStyle.height = "2.2rem";
   }
-
+  mouseMoveHandler = (e) => {
+    if (dragging) {
+      // e.preventDefault();
+      // e.stopPropagation();
+      const newHeight = window.innerHeight - e.clientY;
+      setHeight(newHeight);
+    }
+  };
+  mouseUpHandler = (e) => {
+    // e.preventDefault();
+    // e.stopPropagation();
+    setDragging(false);
+  };
+  const dontDragKids = (e: SyntheticEvent) => e.stopPropagation();
   return (
     <>
-      <div className={containerClass}>
-        <div className="wcf-header-bar">
+      <div style={containerStyle} className={containerClass}>
+        <div
+          className="wcf-header-bar"
+          onMouseDown={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            setDragging(true);
+          }}
+        >
           <span></span>
-          <span
-            className="configurator-toggle material-symbols-outlined"
-            onClick={toggle}
-          >
-            {expansionSymbol}
+          <span onMouseDown={dontDragKids} className="configurator-toggle">
+            <span className=" material-symbols-outlined" onClick={toggle}>
+              {expansionSymbol}
+            </span>
           </span>
           <span
+            onMouseDown={dontDragKids}
             className="configurator-clear-all"
             onClick={props.weaponConfig.Reset}
           >
